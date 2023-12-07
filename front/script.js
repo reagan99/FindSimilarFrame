eb8ed0986903a4335e3c8a7f88f97f02ed4732a3
ycahnn <104204689+ycahnn@users.noreply.github.com>
2023-12-07 23:49:29 +0900

    front

--- front/script.js
+++ front/script.js
@@ -13,19 +13,3 @@ function scroll_up() {
     location.reload(true);
 }
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    // 웹캠 지원 여부 확인
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-      // 웹캠에서 비디오 스트림 얻기
-      navigator.mediaDevices.getUserMedia({ video: true })
-        .then((stream) => {
-          const webcam = document.getElementById('webcam');
-          webcam.srcObject = stream;
-        })
-        .catch((error) => {
-          console.error('웹캠을 사용할 수 없습니다:', error);
-        });
-    } else {
-      console.error('미디어 장치 API가 지원되지 않습니다.');
-    }
-  });
